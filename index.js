4e935d9197cb6a035f907c9a02f603fd7fddb622
Victor Marks <vjmarks@gmail.com>
2019-04-04 00:00:23 -0700

    created a front end with api calls with jquery to our backend

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ let todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
-  res.send('hi there from root route');
+  res.send('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
