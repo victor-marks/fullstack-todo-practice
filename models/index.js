a8139439cad780c783d3ba5ffadc287857940538
Victor Marks <vjmarks@gmail.com>
2019-04-05 16:48:03 -0700

    deploying to heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://vm-fullstack-todo.herokuapp.com/todo-api');
 
 mongoose.Promise = Promise;
 
